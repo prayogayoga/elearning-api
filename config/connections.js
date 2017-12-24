90f7dd0a93034f8edf7889220a9255b5f04c6008
prayogayoga <agungprayoga088@gmail.com>
2017-12-24 19:19:10 +0700

    setup database myelen

--- config/connections.js
+++ config/connections.js
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql@for-sails-0.12 --save                       *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+  koneksiMyElen: {
+     adapter: 'sails-mysql',
+     host: 'localhost',
+     user: 'root', //optional
+     password: '', //optional
+     database: 'myelen' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
